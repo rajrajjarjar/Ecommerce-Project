4beb3d352468b2c35f3dfc948c26998f9ee07a46
Raj <ie.raj.chauhan@gmail.com>
2026-08-19 14:25:23 +0530

    routed pages to the main app now functionality and backend is left

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Homepage from './pages/Homepages'
+import { Routes, Route } from 'react-router'
+import Orders from './pages/Orders'
+import Checkout from './pages/Checkout'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/orders.html" element={<Orders />}></Route>
+        <Route path="/checkout.html" element={<Checkout />}></Route>
+      </Routes>
+
+
     </>
   )
 }
